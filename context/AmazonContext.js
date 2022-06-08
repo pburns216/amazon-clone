300d5c1ebc532c78a8915dd09982dc2583301ee0
Phil <philburns216@gmail.com>
2022-06-08 11:11:46 -0400

    add authentication

---
+++ context/AmazonContext.js
@@ -0,0 +1,56 @@
+import { createContext, useState, useEffect } from "react";
+import { useMoralis, useMoralisQuery } from "react-moralis";
+
+export const AmazonContext = createContext()
+
+export const AmazonProvider = ({children}) =>{
+    const [username, setUsername] = useState('')
+    const [nickname, setNickname] = useState('')
+    const {
+        authenticate,
+        isAuthenticated,
+        enableWeb3,
+        Moralis,
+        user,
+        isWeb3Enabled,
+      } = useMoralis()
+
+    useEffect(()=>{
+        ;(async()=>{
+            if(isAuthenticated){
+                const currentUsername = await user?.get('nickname')
+                setUsername(currentUsername)
+            }
+        })()
+    },[isAuthenticated,user,username])
+
+    const handleSetUsername = () =>{
+        if(user){
+            if(nickname){
+                user.set('nickname',nickname )
+                user.save()
+                setNickname('')
+            }else{
+                console.log("can not set empty nickname")
+            }
+
+        }else{
+            console.log('No user')
+        }
+    }
+
+    return(
+        <AmazonContext.Provider
+            value={{
+           
+    isAuthenticated,
+    nickname,
+    setNickname,
+    username,
+    handleSetUsername
+            }}
+        >
+            {children}
+            </AmazonContext.Provider>
+    )
+}
